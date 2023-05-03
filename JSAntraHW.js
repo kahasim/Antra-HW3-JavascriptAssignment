d14704527f5dc2450202c47abbe3b6fd3bc6a05f
KahasimB <kahasim.brown@outlook.com>
2023-05-03 01:39:45 -0400

    Add files via upload

--- JSAntraHW.js
+++ JSAntraHW.js
@@ -364,7 +364,18 @@ console.log(getSubsets(arr,2))
 // 22. Write a JavaScript function that accepts two arguments, a string and a letter and the function will count the number of occurrences of the specified letter within the string. 
 // Sample arguments: 'microsoft.com', 'o' 
 // Expected output: 3 
+string = "microsoft.com"
+function letteroccurences(string,target){
+let result = ""
+let count = 0
+for(let char of string){
+if(char == target)
+count++
+}
+return count
+}
 console.log("\nProblem #22\n")
+console.log(letteroccurences(string,"o"))
 
 // 23. Write a JavaScript function to find the first not repeated character. 
 // Sample arguments: 'abacddbec' 
